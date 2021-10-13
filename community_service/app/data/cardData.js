6314f85981e33bcdcaafa533e955e1e32ffc1f49
Jay Lee <jay.lee031515@gmail.cm>
2021-10-12 18:16:14 -0700

    Added an app folder to contain all app related items, added a data folder to house all data needed for the app, and moved the digital assets to the app folder(added more images to be used for the app).

---
+++ community_service/app/data/cardData.js
@@ -0,0 +1,33 @@
+const cardData =
+  [
+    {
+      id: 1,
+      title: 'Bark Bark Dog Park',
+      date: '2021-10-20',
+      image: require('../assets/DogLogo.jpeg'),
+      location: 'tbd',
+    },
+    {
+      id: 2,
+      title: 'Community Dog Walk',
+      date: '2021-10-20',
+      image: require('../assets/SecondDogLogo.jpeg'),
+      location: 'tbd',
+    },
+    {
+      id: 3,
+      title: 'Strut Your Mutt',
+      date: '2021-10-20',
+      image: require('../assets/ThirdDogLogo.jpeg'),
+      location: 'tbd',
+    },
+    {
+      id: 4,
+      title: 'No Tricks Just Treats',
+      date: '2021-10-20',
+      image: require('../assets/FourthDogLogo.jpeg'),
+      location: 'tbd',
+    },
+  ]
+
+export default cardData;
---
+++ community_service/app/data/eventData.js
@@ -0,0 +1,67 @@
+const eventData =   [
+  {
+    id: 1,
+    title: 'Bark Bark Dog Park',
+    date: '2021-10-20',
+    description: ,
+    contactPhone: '1-800-Run-Dogo',
+    contactName: 'Shop With Rob',
+    image: [
+      require('../assets/DogLogo.jpeg'),
+      require('../assets/DogPicOne.jpeg'),
+      require('../assets/DogPicTwo.jpeg'),
+      require('../assets/DogPicThree.jpeg')
+    ],
+    location: 'tbd',
+  },
+  {
+    id: 2,
+    title: 'Community Dog Walk',
+    date: '2021-10-20',
+    description: ,
+    contactPhone: '1-800-Pug-Walk',
+    contactName: 'SDC',
+    image: [
+      require('../assets/SecondDogLogo.jpeg'),
+      require('../assets/SecondDogPicOne.jpeg'),
+      require('../assets/SecondDogPicTwo.jpg'),
+      require('../assets/SecondDogPicThree.jpeg'),
+      require('../assets/SecondDogPicFour.png')
+    ],
+    location: 'tbd',
+  },
+  {
+    id: 3,
+    title: 'Strut Your Mutt',
+    date: '2021-10-20',
+    description: 'This ones for you Tim',
+    contactPhone: '1-800-Pos-Gres',
+    contactName: 'MVP',
+    image: [
+      require('../assets/ThirdDogLogo.jpeg'),
+      require('../assets/ThirdDogPicOne.jpeg'),
+      require('../assets/ThirdDogPicTwo.jpeg'),
+      require('../assets/ThirdDogPicThree.jpeg'),
+      require('../assets/ThirdDogPicFour.png')
+    ],
+    location: 'tbd',
+  },
+  {
+    id: 4,
+    title: 'No Tricks Just Treats',
+    date: '2021-10-20',
+    description: 'Why is creating data so hard?',
+    contactPhone: '1-800-867-5309',
+    contactName: 'Blue Ocean',
+    image: [
+      require('../assets/FourthDogLogo.jpeg'),
+      require('../assets/FourDogPicOne.jpeg'),
+      require('../assets/FourDogPicTwo.jpeg'),
+      require('../assets/FourDogPicThree.jpeg'),
+      require('../assets/FourDogPicFour.png')
+    ],
+    location: 'tbd',
+  },
+]
+
+export default eventData;
