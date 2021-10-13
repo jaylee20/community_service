af6d98115ca5e2eb9507f712b271e4c7739a369f
Jay Lee <jay.lee031515@gmail.cm>
2021-10-12 21:07:20 -0700

    Removed boiler plate code from Expo in App.js

--- community_service/App.js
+++ community_service/App.js
@@ -1,10 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthNavigator from './app/navigation/AuthNavigator';
-import LottieView from 'lottie-react-native';
 
 console.disableYellowBox = true
 
@@ -17,12 +15,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
